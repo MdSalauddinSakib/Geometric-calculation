b1901ed2455cad158ec8729ab6c378d8581ddcce
Md Salauddin Sakib <salauddinsakib99@gmail.com>
2024-04-10 21:40:07 +0600

    triangle area calculation JS added done

--- scripts/triangle-calculate.js
+++ scripts/triangle-calculate.js
@@ -1,3 +1,27 @@
+/**
+ * objective 
+ * */ 
+
+
+
 function calculateTriangleArea(){
-    console.log('inside function')
+    // get triangle base value
+    const triangleBaseInput = document.getElementById('triangle-base');
+    const triangleBaseText = triangleBaseInput.value;
+    const base = parseFloat(triangleBaseText);
+    console.log(base);
+
+    // get triangle height value
+    const triangleHeightValue= document.getElementById('triangle-height');
+    const triangleHeightText = triangleHeightValue.value;
+    const height = parseFloat(triangleHeightText);
+    console.log(height);
+
+    // calculate triangle area
+    const triangleArea = 0.5 * base * height;
+    console.log('calculate of the triangle is:', triangleArea);
+
+    // display triangle aria
+    const triangleAreaSpan = document.getElementById('triangle-area');
+    triangleAreaSpan.innerText = triangleArea;
 }
